128049a3178c9ded0d968a9573a513585a4f95f7
Matt Melling <matt@kobowi.co.uk>
2013-08-06 20:52:59 +0100

    moved settings to media editor group

--- Handlers/DropboxSettingsPartHandler.cs
+++ Handlers/DropboxSettingsPartHandler.cs
@@ -1,12 +1,17 @@
 ﻿using Kobowi.Dropbox.Models;
 using Orchard.ContentManagement.Handlers;
 using Orchard.Data;
+using Orchard.Localization;
 
 namespace Kobowi.Dropbox.Handlers {
     public class DropboxSettingsPartHandler : ContentHandler {
+        public Localizer T { get; set; }
+
         public DropboxSettingsPartHandler(IRepository<DropboxSettingsPartRecord> repository) {
+            T = NullLocalizer.Instance;
             Filters.Add(StorageFilter.For(repository));
             Filters.Add(new ActivatingFilter<DropboxSettingsPart>("Site"));
+            Filters.Add(new TemplateFilterForPart<DropboxSettingsPart>("DropboxSettings", "Parts.DropboxSettingsPart", "media"));
         }
     }
 }
